91801111f8a4f3f0836ae579c897433907790616
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-24 16:05:58 +0200

    2nd task has finished.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const throtled = throttle(getCurrentTime, 1000);
+
+
+player.on('timeupdate', throtled);
+player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+
+
+    
+function getCurrentTime(obj) {
+    const { seconds } = obj;
+    localStorage.setItem('videoplayer-current-time',JSON.stringify(`${seconds}`))
+
+}
+
+
+
 
