eb9adeaf88d3b66fc0f09dd121d672514fb1b6cf
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-24 22:57:23 +0200

    finished task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle'
+
+const ourFormREF = document.querySelector('.feedback-form');
+const ourForminfo = {};
+const emailInput = document.querySelector('[name="email"]')
+const messageInput = document.querySelector('[name="message"]')
+const parsedObj = JSON.parse(localStorage.getItem('feedback-form-state')) || {email: ``, message:``};
+
+
+
+emailInput.value = `${parsedObj.email}`;
+messageInput.value = `${parsedObj.message}`;
+
+
+ourFormREF.addEventListener('input', throttle(onFormInput, 500));
+ourFormREF.addEventListener('submit', onFormSubmit);
+
+
+
+
+function onFormInput(e) {
+    console.log(ourForminfo);
+    ourForminfo[e.target.name] = e.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(ourForminfo))
+    
+
+};
+
+function onFormSubmit(e) {
+    console.log(parsedObj);
+    localStorage.clear()
+}
 
