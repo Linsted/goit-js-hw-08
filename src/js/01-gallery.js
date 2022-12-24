01d1b39fcc8b2933b08108a59cd9a7c3e2f44a9a
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-24 12:59:16 +0200

    1st task has been finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryListREF = document.querySelector('.gallery');
+galleryListREF.innerHTML = crateMarkup(galleryItems);
+
+function crateMarkup(items) {
+    return items.map(item => `<div><a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></div>`).join('')
+};
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: `alt`, captionDelay: 250,});
