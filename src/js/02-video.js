3ce39c857b235b39485af87b86c078e728befa8d
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-25 14:35:31 +0200

    fixed task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,10 +5,13 @@ import throttle from 'lodash.throttle'
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const throtled = throttle(getCurrentTime, 1000);
+const savedTime = localStorage.getItem('videoplayer-current-time');
 
 
 player.on('timeupdate', throtled);
-player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+if (savedTime) { player.setCurrentTime(JSON.parse(savedTime)) };
+//  чи можна було б зробити отак ?  без if   player.setCurrentTime(JSON.parse(savedTime) || 0 )
+
 
 
     
